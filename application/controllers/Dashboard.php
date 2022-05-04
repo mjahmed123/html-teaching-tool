9a16643cbc59e23b03a4b09ccc6e708a68b48084
mjahmed123 <94318179+mjahmed123@users.noreply.github.com>
2022-05-04 16:20:31 +0100

    Retrieves category score from the database

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -8,17 +8,23 @@ class Dashboard extends CI_Controller {
 		$this->load->helper('html');
 		$this->load->helper('url');
 		$this->load->model('user_model');
+		$this->load->model('completed_quizzes_model');
+
 	}
+
 
 	public function tags() {
 		$this->checkIfLoggedIn();
+
+		
 		$this->load->view('pages/categories/tags_page');
 	}
-
+	
 	public function index()
 	{
 		$this->checkIfLoggedIn();
-		$this->load->view('pages/dashboard_page');
+		$data['completed_quizzes'] = $this->completed_quizzes_model->getByUserId($_SESSION['user_id']);
+		$this->load->view('pages/dashboard_page', $data);
 	}
 	private function checkIfLoggedIn() {
 		// redirect to login if not logged in

4edc5d83444c9cbb6c8985f08f225abedbd400d9
mjahmed123 <94318179+mjahmed123@users.noreply.github.com>
2022-05-14 11:58:39 +0100

    Moved code to show users score to its own function

--- application/views/pages/dashboard_page.php
+++ application/views/pages/dashboard_page.php
@@ -6,6 +6,29 @@
 		echo link_tag('css/headerStyle.css');
 		echo link_tag('css/footerStyle.css');
 		echo link_tag('css/dashboardStyle.css');
+
+
+
+		function percentage($completed_quizzes, $category_id) {
+			$quiz_completed = null;
+			for ($i=0; $i < count($completed_quizzes); $i++) {
+				if ($completed_quizzes[$i]->category_id == $category_id) {
+					$quiz_completed = $completed_quizzes[$i];
+					break;
+				}
+			}
+
+			if (empty($quiz_completed)) {
+				return "";
+			} 
+			$score = $quiz_completed->score;
+			$correct_answers = explode("/" , $score)[0];
+			$total_answers = explode("/" , $score)[1];
+			$percentage = ($correct_answers / $total_answers ) * 100;
+			return $percentage . "%";
+		}
+
+
 	?>
 	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 	<script src="<?php echo site_url('js/darkMode.js'); ?>"></script>
@@ -21,22 +44,13 @@
 			<a href="/categories/tags" class="category">
 				<div class="material-icons">code</div>
 				Tags
-				<div class="score"><?php 
-					if (empty($completed_quizzes[0])) {
-						echo "";
-					} else {
-						$score = $completed_quizzes[0]->score;
-						$correct_answers = explode("/" , $score)[0];
-						$total_answers = explode("/" , $score)[1];
-						$percentage = ($correct_answers / $total_answers ) * 100;
-						echo $percentage . "%";
-					}
-				?></div>
+				<div class="score"><?php echo percentage($completed_quizzes, 0) ?></div>
 			</a>
 			<a href="/categories/attributes" class="category">
 			<div class="material-icons">tune</div>
 
 				Attributes
+				<div class="score"><?php echo percentage($completed_quizzes, 1) ?></div>
 
 			</a>
 			<a href="/categories/styles" class="category">

1e245f4b00b3a3575eb66b098976c81d865a5b1c
mjahmed123 <94318179+mjahmed123@users.noreply.github.com>
2022-05-14 12:02:13 +0100

    Updated questions and page redirect when submited

--- application/views/pages/quizzes/identify_attributes_page.php
+++ application/views/pages/quizzes/identify_attributes_page.php
@@ -40,52 +40,52 @@
     }
   ?>
 	<div class="container quiz">
-    <form method="post" action="<?php echo base_url(); ?>quizzes/submit_tags?id=1">
+    <form method="post" action="<?php echo base_url(); ?>quizzes/submit_attributes?id=2">
       <!--Question 1-->
       <div class="question <?php output_answer_status($answer_status, 1) ?>" id="question-1">
         <?php create_header($answer_status, 1) ?>
-        <p><?php question_number(1) ?>What is a HTML tag?</p>
+        <p><?php question_number(1) ?>What is an attribute in HTML?</p>
 
         <div class="answer <?php output_is_correct_answer($correct_answers, 1, 1) ?>">
           <input type="radio" id="a1" name="q1a1" value="1">
-          <label for="a1">A keyword that is used to describe the format of a webpage</label>
+          <label for="a1">A quality regarded as a characteristic of someone or something.</label>
         </div>
         <div class="answer <?php output_is_correct_answer($correct_answers, 1, 2) ?>">
           <input type="radio" id="a2" name="q1a2" value="2">
-          <label for="a2">A tag used to indicate the price of an item</label> 
+          <label for="a2">An attribute is a value that is specified within an element's start tag after it's tag name.</label> 
         </div>
         <div class="answer <?php output_is_correct_answer($correct_answers, 1, 3) ?>">
           <input type="radio" id="a3" name="q1a3" value="3">
-          <label for="a3">A word you use when playing tag</label>
+          <label for="a3">Something that is regarding as being caused by someone or something.</label>
         </div>
       </div>
 
       <!--Question 2-->
       <div class="question <?php output_answer_status($answer_status, 2) ?>" id="question-2">
       <?php create_header($answer_status, 2) ?>
-        <p><?php question_number(2) ?>Which of these have the correct syntax for a <code>h1</code> tag?</p>
+        <p><?php question_number(2) ?>Which of the following correctly define the <code>href</code> attribute in an <code>&lt;a&gt;</code> tag?</p>
 
         <div class="answer <?php output_is_correct_answer($correct_answers, 2, 1) ?>">
           <input type="radio" id="a4" name="q2a1" value="1">
-          <label for="a4"><code>&lt;h1&gt;Example Header&lt;/h1&gt;</code></label>
+          <label for="a4"><code>&lt;a href-"https://www.google.co.uk"&gt;Example Link&lt;/a&gt;</code></label>
         </div>
         <div class="answer <?php output_is_correct_answer($correct_answers, 2, 2) ?>">
           <input type="radio"id="a5" name="q2a2" value="2">
-          <label for="a5"><code>&lt;h1&gt;Example Header&lt;h1&gt;</code></label> 
+          <label for="a5"><code>&lt;a "https://www.google.co.uk"&gt;Example Link&lt;/a&gt;</code></label> 
         </div>
         <div class="answer <?php output_is_correct_answer($correct_answers, 3, 3) ?>">
           <input type="radio" id="a6" name="q2a3" value="3">
-          <label for="a6"><code>&lt;/h1&gt;Example Header&lt;/h1&gt;</code></label>
+          <label for="a6"><code>&lt;a href="https://www.google.co.uk"&gt;Example Link&lt;/a&gt;</code></label>
         </div>
       </div>
 
       <!--Question 3-->
       <div class="question <?php output_answer_status($answer_status, 3) ?>" id="question-3">
       <?php create_header($answer_status, 3) ?>
-        <p><?php question_number(3) ?>Complete the syntax of the code given below:</p>
+        <p><?php question_number(3) ?>Complete the syntax of the following code so it sets the width of the image to 500px:</p>
 
         <code class="example">
-          &lt;p&gt;This is a sample paragraph.<u>&nbsp;&nbsp;&nbsp;</u>
+          &lt;img src="image.png" width="<u>&nbsp;&nbsp;</u>" height="450"&gt;
         </code>
         <input value="<?php answer($correct_answers, 3) ?>" id="textInput" class="short " name="q3a1_i" type="text" placeholder="Answer">
       </div>
@@ -93,38 +93,19 @@
       <!--Question 4-->
       <div class="question <?php output_answer_status($answer_status, 4) ?>" id="question-4">
       <?php create_header($answer_status, 4) ?>
-        <p><?php question_number(4) ?>Which of the following contain all the metadata for a webpage?</p>
+        <p><?php question_number(4) ?>What standard does an attribute follow?</p>
 
         <div class="answer <?php output_is_correct_answer($correct_answers, 4, 1) ?>">
           <input type="radio" id="a7" name="q4a1" value="1">
-          <label for="a7"><code>&lt;footer&gt;</code></label>
+          <label for="a7">Driver/Navigator</label>
         </div>
         <div class="answer <?php output_is_correct_answer($correct_answers, 4, 2) ?>">
           <input type="radio"id="a8" name="q4a2" value="2">
-          <label for="a8"><code>&lt;body&gt;</code></label> 
+          <label for="a8">Ping-Pong</label> 
         </div>
         <div class="answer <?php output_is_correct_answer($correct_answers, 4, 3) ?>">
           <input type="radio" id="a9" name="q4a3" value="3">
-          <label for="a9"><code>&lt;head&gt;</code></label>
-        </div>
-      </div>
-
-      <!--Question 5-->
-      <div class="question <?php output_answer_status($answer_status, 5) ?>" id="question-5">
-      <?php create_header($answer_status, 5) ?>
-        <p><?php question_number(5) ?>Which of the following is the correct way to create a comment?</p>
-
-        <div class="answer <?php output_is_correct_answer($correct_answers, 5, 1) ?>">
-          <input type="radio" id="a10" name="q5a1" value="1">
-          <label for="a10"><code>&lt;--This is an example comment--&gt;</code></label>
-        </div>
-        <div class="answer <?php output_is_correct_answer($correct_answers, 5, 2) ?>">
-          <input type="radio"id="a11" name="q5a2" value="2">
-          <label for="a11"><code>&lt;!--This is an example comment--!&gt;</code></label> 
-        </div>
-        <div class="answer <?php output_is_correct_answer($correct_answers, 5, 3) ?>">
-          <input type="radio" id="a12" name="q5a3" value="3">
-          <label for="a12"><code>&lt;!--This is an example comment--&gt;</code></label>
+          <label for="a9">Name/Value</label>
         </div>
       </div>
 

7732300e0f9eb91f15a475387bda71388116ebfa
mjahmed123 <94318179+mjahmed123@users.noreply.github.com>
2022-05-14 11:53:25 +0100

    Added second quiz and questions

--- quizzes.sql
+++ quizzes.sql
@@ -7,6 +7,8 @@ CREATE TABLE quizzes (
     PRIMARY KEY (id)
 );
 
+-- Quiz 1 - Identifying tags and their structure --
+
 -- What is a HTML tag?
 INSERT INTO quizzes (quiz_id, question_id, answer_id) VALUES (1, 1, 1);
 
@@ -18,3 +20,22 @@ INSERT INTO quizzes (quiz_id, question_id, answer) VALUES (1, 3, "</p>");
 
 -- Which of the following contain all the metadata for a webpage?
 INSERT INTO quizzes (quiz_id, question_id, answer_id) VALUES (1, 4, 3);
+
+-- Which of the following is the correct way to create a comment?
+INSERT INTO quizzes (quiz_id, question_id, answer_id) VALUES (1, 5, 3);
+
+
+
+-- Quiz 2 - Identifying and defining elements attributes within elements --
+
+-- What is an attribute?
+INSERT INTO quizzes(quiz_id, question_id, answer_id) VALUES (2, 1, 2);
+
+-- Which of the following define href in an a tag?
+INSERT INTO quizzes(quiz_id, question_id, answer_id) VALUES (2, 2, 3);
+
+-- Complete the syntax of the following code
+INSERT INTO quizzes(quiz_id, question_id, answer) VALUES (2, 3, "500");
+
+-- What standard does an attribute follow?
+INSERT INTO quizzes(quiz_id, question_id, answer_id) VALUES (2, 4, 3);

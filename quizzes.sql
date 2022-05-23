2596ebff08211000685f5bd897311bd0c3f1d828
mjahmed123 <94318179+mjahmed123@users.noreply.github.com>
2022-05-23 18:12:23 +0100

    Added remaining quizzes to quizzes table

--- quizzes.sql
+++ quizzes.sql
@@ -39,3 +39,87 @@ INSERT INTO quizzes(quiz_id, question_id, answer) VALUES (2, 3, "500");
 
 -- What standard does an attribute follow?
 INSERT INTO quizzes(quiz_id, question_id, answer_id) VALUES (2, 4, 3);
+
+
+
+-- Quiz 3 - Defining style types, selectors and properties --
+
+-- What are styles?
+INSERT INTO quizzes(quiz_id, question_id, answer_id) VALUES (3, 1, 1);
+
+-- What are the different types of styling?
+INSERT INTO quizzes(quiz_id, question_id, answer_id) VALUES (3, 2, 3);
+
+-- Complete the syntax of the following code
+INSERT INTO quizzes(quiz_id, question_id, answer) VALUES (3, 3, 'stylesheet');
+
+-- In which tag should the above link tag be placed in between to link an external css file?
+INSERT INTO quizzes(quiz_id, question_id, answer_id) VALUES (3, 4, 2);
+
+-- Which of the following best describes inline styling?
+INSERT INTO quizzes(quiz_id, question_id, answer_id) VALUES (3, 5, 3);
+
+-- Which of the following best describes internal styling?
+INSERT INTO quizzes(quiz_id, question_id, answer_id) VALUES (3, 6, 2);
+
+-- Which of the following best describes external styling?
+INSERT INTO quizzes(quiz_id, question_id, answer_id) VALUES (3, 7, 1);
+
+-- Complete the syntax of the following code:
+INSERT INTO quizzes(quiz_id, question_id, answer) VALUES (3, 8, 'green');
+
+
+
+-- Quiz 4 - Identifying forms, their attributes and elements --
+
+-- What is a form in HTML?
+INSERT INTO quizzes(quiz_id, question_id, answer_id) VALUES (4, 1, 1);
+
+-- Which of the following is one of the many input elements contained in any form?
+INSERT INTO quizzes(quiz_id, question_id, answer_id) VALUES (4, 2, 1);
+
+-- Complete the syntax of the following code:
+INSERT INTO quizzes(quiz_id, question_id, answer) VALUES (4, 3, 'text');
+
+-- Which of these best describe a radio button?
+INSERT INTO quizzes(quiz_id, question_id, answer_id) VALUES (4, 4, 2);
+
+-- Which of these best describe checkboxes?
+INSERT INTO quizzes(quiz_id, question_id, answer_id) VALUES (4, 5, 3);
+
+-- Which of these best describe a textbox?
+INSERT INTO quizzes(quiz_id, question_id, answer_id) VALUES (4, 6, 1);
+
+-- Complete the syntax of the following code:
+INSERT INTO quizzes(quiz_id, question_id, answer) VALUES (4, 7, 'complete');
+
+
+
+-- Quiz 5 - Identifying types of media and how to use them --
+
+-- What is multimedia?
+INSERT INTO quizzes(quiz_id, question_id, answer_id) VALUES (5, 1, 3);
+
+-- Which of the following is one of the many types of multimedia?
+INSERT INTO quizzes(quiz_id, question_id, answer_id) VALUES (5, 2, 2);
+
+-- Complete the syntax of the following code:
+INSERT INTO quizzes(quiz_id, question_id, answer) VALUES (5, 3, '600');
+
+-- Which of the following best describes a <video> tag?
+INSERT INTO quizzes(quiz_id, question_id, answer_id) VALUES (5, 4, 3);
+
+-- Which of the following best describes a <iframe> tag?
+INSERT INTO quizzes(quiz_id, question_id, answer_id) VALUES (5, 5, 1);
+
+-- Which of the following best describes a <audio> tag?
+INSERT INTO quizzes(quiz_id, question_id, answer_id) VALUES (5, 6, 2);
+
+-- Complete the syntax of the following code:
+INSERT INTO quizzes(quiz_id, question_id, answer) VALUES (5, 7, 'autoplay');
+
+-- Complete the syntax of the following code:
+INSERT INTO quizzes(quiz_id, question_id, answer) VALUES (5, 8, 'controls');
+
+-- Complete the syntax of the following code:
+INSERT INTO quizzes(quiz_id, question_id, answer) VALUES (5, 9, 'loop');
